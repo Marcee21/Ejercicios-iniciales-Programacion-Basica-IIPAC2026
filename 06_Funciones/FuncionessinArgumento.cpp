509d8c8b73a7f0f0f6a87e872738dfdc1907075f
Dilia Castellanos <marcelasoriano504@gmail.com>
2026-06-11 17:06:55 -0600

    Funciones sin argumentos

---
+++ 06_Funciones/FuncionessinArgumento.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void saludar() {
+	std::cout << "Hola! Bienvenido.\n";
+}
+
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	
+	saludar();
+	
+
+	
+	
+	return 0;
+    }
